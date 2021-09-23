b623dbd110ac2c987f690751bd22d1f1e75ccdb1
Alef Gaigher <37152658+AlefGaigher@users.noreply.github.com>
2021-09-23 14:20:17 -0300

    Primeiro Commit
    Classes Iniciais para primeiro aprendizado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aprendendo_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           ContaCorrente contaCorrente = new ContaCorrente();
+           contaCorrente.titular = "Gabriela";
+           contaCorrente.agencia = 856;
+           contaCorrente.numero = 837475;
+           contaCorrente.saldo = 56.4;
+        }
+    }
+}
